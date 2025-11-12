8a16b6f779265494ed71e1194a7eca6774a05746
Marcos Aleixo <maknytz@gmail.com>
2025-11-11 21:12:30 -0300

    Criando a listagem de clientes

---
+++ clientes.php
@@ -0,0 +1,40 @@
+<?php 
+    include('conexao.php');
+    $sql = "SELECT * FROM clientes";
+    $query_clientes = $mysqli->query($sql) or die
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Clientes</title>
+</head>
+<body>
+    <h1>Lista de Clientes</h1>
+    <p>Estes são os clientes cadastrados no sistema:</p>
+    <table>
+        <thead>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Telefone</th>
+            <th>Nascimento</th>
+            <th>Data</th>
+            <th></th>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
